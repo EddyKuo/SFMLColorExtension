98d0c238cee153756bbed222fd8eeca8338e32b9
Eddy Kuo <itsuki520@gmail.com>
2019-06-30 00:02:15 +0800

    first commit
    sf::Color extension

---
+++ ColorEx.h
@@ -0,0 +1,199 @@
+#ifndef __SF_COLOR_EX_H__
+#define __SF_COLOR_EX_H__
+
+#include <SFML/Graphics/Color.hpp>
+
+namespace sf {
+    class ColorEx : public sf::Color {
+
+    private:
+        ColorEx() {};
+        ~ColorEx() {};
+    public:
+        static const sf::Color ActiveBorder;
+        static const sf::Color ActiveCaption;
+        static const sf::Color ActiveCaptionText;
+        static const sf::Color AppWorkspace;
+        static const sf::Color Control;
+        static const sf::Color ControlDark;
+        static const sf::Color ControlDarkDark;
+        static const sf::Color ControlLight;
+        static const sf::Color ControlLightLight;
+        static const sf::Color ControlText;
+        static const sf::Color Desktop;
+        static const sf::Color GrayText;
+        static const sf::Color Highlight;
+        static const sf::Color HighlightText;
+        static const sf::Color HotTrack;
+        static const sf::Color InactiveBorder;
+        static const sf::Color InactiveCaption;
+        static const sf::Color InactiveCaptionText;
+        static const sf::Color Info;
+        static const sf::Color InfoText;
+        static const sf::Color Menu;
+        static const sf::Color MenuText;
+        static const sf::Color ScrollBar;
+        static const sf::Color Window;
+        static const sf::Color WindowFrame;
+        static const sf::Color WindowText;
+        static const sf::Color Transparent;
+        static const sf::Color AliceBlue;
+        static const sf::Color AntiqueWhite;
+        static const sf::Color Aqua;
+        static const sf::Color Aquamarine;
+        static const sf::Color Azure;
+        static const sf::Color Beige;
+        static const sf::Color Bisque;
+        static const sf::Color Black;
+        static const sf::Color BlanchedAlmond;
+        static const sf::Color Blue;
+        static const sf::Color BlueViolet;
+        static const sf::Color Brown;
+        static const sf::Color BurlyWood;
+        static const sf::Color CadetBlue;
+        static const sf::Color Chartreuse;
+        static const sf::Color Chocolate;
+        static const sf::Color Coral;
+        static const sf::Color CornflowerBlue;
+        static const sf::Color Cornsilk;
+        static const sf::Color Crimson;
+        static const sf::Color Cyan;
+        static const sf::Color DarkBlue;
+        static const sf::Color DarkCyan;
+        static const sf::Color DarkGoldenrod;
+        static const sf::Color DarkGray;
+        static const sf::Color DarkGreen;
+        static const sf::Color DarkKhaki;
+        static const sf::Color DarkMagenta;
+        static const sf::Color DarkOliveGreen;
+        static const sf::Color DarkOrange;
+        static const sf::Color DarkOrchid;
+        static const sf::Color DarkRed;
+        static const sf::Color DarkSalmon;
+        static const sf::Color DarkSeaGreen;
+        static const sf::Color DarkSlateBlue;
+        static const sf::Color DarkSlateGray;
+        static const sf::Color DarkTurquoise;
+        static const sf::Color DarkViolet;
+        static const sf::Color DeepPink;
+        static const sf::Color DeepSkyBlue;
+        static const sf::Color DimGray;
+        static const sf::Color DodgerBlue;
+        static const sf::Color Firebrick;
+        static const sf::Color FloralWhite;
+        static const sf::Color ForestGreen;
+        static const sf::Color Fuchsia;
+        static const sf::Color Gainsboro;
+        static const sf::Color GhostWhite;
+        static const sf::Color Gold;
+        static const sf::Color Goldenrod;
+        static const sf::Color Gray;
+        static const sf::Color Green;
+        static const sf::Color GreenYellow;
+        static const sf::Color Honeydew;
+        static const sf::Color HotPink;
+        static const sf::Color IndianRed;
+        static const sf::Color Indigo;
+        static const sf::Color Ivory;
+        static const sf::Color Khaki;
+        static const sf::Color Lavender;
+        static const sf::Color LavenderBlush;
+        static const sf::Color LawnGreen;
+        static const sf::Color LemonChiffon;
+        static const sf::Color LightBlue;
+        static const sf::Color LightCoral;
+        static const sf::Color LightCyan;
+        static const sf::Color LightGoldenrodYellow;
+        static const sf::Color LightGray;
+        static const sf::Color LightGreen;
+        static const sf::Color LightPink;
+        static const sf::Color LightSalmon;
+        static const sf::Color LightSeaGreen;
+        static const sf::Color LightSkyBlue;
+        static const sf::Color LightSlateGray;
+        static const sf::Color LightSteelBlue;
+        static const sf::Color LightYellow;
+        static const sf::Color Lime;
+        static const sf::Color LimeGreen;
+        static const sf::Color Linen;
+        static const sf::Color Magenta;
+        static const sf::Color Maroon;
+        static const sf::Color MediumAquamarine;
+        static const sf::Color MediumBlue;
+        static const sf::Color MediumOrchid;
+        static const sf::Color MediumPurple;
+        static const sf::Color MediumSeaGreen;
+        static const sf::Color MediumSlateBlue;
+        static const sf::Color MediumSpringGreen;
+        static const sf::Color MediumTurquoise;
+        static const sf::Color MediumVioletRed;
+        static const sf::Color MidnightBlue;
+        static const sf::Color MintCream;
+        static const sf::Color MistyRose;
+        static const sf::Color Moccasin;
+        static const sf::Color NavajoWhite;
+        static const sf::Color Navy;
+        static const sf::Color OldLace;
+        static const sf::Color Olive;
+        static const sf::Color OliveDrab;
+        static const sf::Color Orange;
+        static const sf::Color OrangeRed;
+        static const sf::Color Orchid;
+        static const sf::Color PaleGoldenrod;
+        static const sf::Color PaleGreen;
+        static const sf::Color PaleTurquoise;
+        static const sf::Color PaleVioletRed;
+        static const sf::Color PapayaWhip;
+        static const sf::Color PeachPuff;
+        static const sf::Color Peru;
+        static const sf::Color Pink;
+        static const sf::Color Plum;
+        static const sf::Color PowderBlue;
+        static const sf::Color Purple;
+        static const sf::Color Red;
+        static const sf::Color RosyBrown;
+        static const sf::Color RoyalBlue;
+        static const sf::Color SaddleBrown;
+        static const sf::Color Salmon;
+        static const sf::Color SandyBrown;
+        static const sf::Color SeaGreen;
+        static const sf::Color SeaShell;
+        static const sf::Color Sienna;
+        static const sf::Color Silver;
+        static const sf::Color SkyBlue;
+        static const sf::Color SlateBlue;
+        static const sf::Color SlateGray;
+        static const sf::Color Snow;
+        static const sf::Color SpringGreen;
+        static const sf::Color SteelBlue;
+        static const sf::Color Tan;
+        static const sf::Color Teal;
+        static const sf::Color Thistle;
+        static const sf::Color Tomato;
+        static const sf::Color Turquoise;
+        static const sf::Color Violet;
+        static const sf::Color Wheat;
+        static const sf::Color White;
+        static const sf::Color WhiteSmoke;
+        static const sf::Color Yellow;
+        static const sf::Color YellowGreen;
+        static const sf::Color ButtonFace;
+        static const sf::Color ButtonHighlight;
+        static const sf::Color ButtonShadow;
+        static const sf::Color GradientActiveCaption;
+        static const sf::Color GradientInactiveCaption;
+        static const sf::Color MenuBar;
+        static const sf::Color MenuHighlight;
+
+
+    private:
+    };
+}
+
+
+
+
+
+
+
+#endif
